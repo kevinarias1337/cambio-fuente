855936c5406595b60cfdef8f86bf4e633e62eefd
Kevin Arias <kevinlh1337@gmail.com>
2019-09-02 20:29:40 -0500

    Se sube el proyecto a github/rama remota.

---
+++ codigofuenteletra/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace codigofuenteletra
+{
+    partial class programa
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(programa));
+            this.label1 = new System.Windows.Forms.Label();
+            this.nombretxt = new System.Windows.Forms.TextBox();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.btncontinuar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label1.Location = new System.Drawing.Point(168, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese su nombre:";
+            // 
+            // nombretxt
+            // 
+            this.nombretxt.Location = new System.Drawing.Point(373, 65);
+            this.nombretxt.Name = "nombretxt";
+            this.nombretxt.Size = new System.Drawing.Size(146, 20);
+            this.nombretxt.TabIndex = 1;
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Location = new System.Drawing.Point(370, 119);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(35, 13);
+            this.lblresultado.TabIndex = 2;
+            this.lblresultado.Text = "label2";
+            // 
+            // btncontinuar
+            // 
+            this.btncontinuar.Location = new System.Drawing.Point(525, 65);
+            this.btncontinuar.Name = "btncontinuar";
+            this.btncontinuar.Size = new System.Drawing.Size(91, 19);
+            this.btncontinuar.TabIndex = 3;
+            this.btncontinuar.Text = "Continuar";
+            this.btncontinuar.UseVisualStyleBackColor = true;
+            this.btncontinuar.Click += new System.EventHandler(this.Btncontinuar_Click);
+            // 
+            // programa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 251);
+            this.Controls.Add(this.btncontinuar);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.nombretxt);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "programa";
+            this.Text = "Fuente de letra y color codificado.";
+            this.Load += new System.EventHandler(this.Programa_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nombretxt;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.Button btncontinuar;
+    }
+}
+
---
+++ codigofuenteletra/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace codigofuenteletra
+{
+    public partial class programa : Form
+    {
+        public programa()
+        {
+            InitializeComponent();
+        }
+
+        private void Programa_Load(object sender, EventArgs e)
+        {
+            lblresultado.Visible = false;
+            nombretxt.Focus();
+        }
+
+        private void Btncontinuar_Click(object sender, EventArgs e)
+        {
+            lblresultado.Text = (nombretxt.Text);
+            
+            lblresultado.Font = new Font(FontFamily.GenericMonospace, 30.0F,
+            FontStyle.Italic | FontStyle.Underline | FontStyle.Strikeout, GraphicsUnit.Pixel);
+            lblresultado.ForeColor = Color.Red;
+            nombretxt.Text = "";
+            nombretxt.Focus();
+            lblresultado.Visible = true;
+        }
+    }
+}
